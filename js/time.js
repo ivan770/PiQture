27059c066cb05a926cb5163bbb489ec1e03c3dec
ivan770 <leshenko.ivan770@gmail.com>
2018-02-17 17:12:56 +0200

    Added time-based names for quick capture

---
+++ js/time.js
@@ -0,0 +1,10 @@
+function piqture_time () {
+  now = new Date();
+  year = "" + now.getFullYear();
+  month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+  day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+  hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+  return year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second;
+}
