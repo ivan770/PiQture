9807dbf6f0bba532617376771fad9cdfde041be8
ivan770 <leshenko.ivan770@gmail.com>
2018-02-18 14:10:53 +0200

    Version change, tray update

--- js/tray.js
+++ js/tray.js
@@ -14,6 +14,34 @@ let trayIcon = new Tray(path.join(__dirname, 'assets/icons/icon.png'))
             },
 
             {
+               label: 'Screenshot',
+               click: function () {
+                  start(path.join(os.homedir(), piqture_time()+".png"))
+               },
+               submenu: [
+                 {
+                 label: 'Quick capture',
+                 click: function () {
+                    start(path.join(os.homedir(), piqture_time()+".png"))
+                 }
+               },
+                 {
+                   label: 'Choose path',
+                   click: function () {
+                     ipc.send('save-dialog')
+                     ipc.on('saved-file', function (event, pathFile) {
+                     if (!pathFile) pathFile = 'Clipboard'
+                     screenshotMsg.placeholder = `Path selected: ${pathFile}`
+                     start(pathFile)
+                   })
+                   }
+                 }
+                        ]
+            },
+            {
+              type: 'separator'
+            },
+            {
                label: 'Exit',
                click: function () {
                   exit()
@@ -23,3 +51,4 @@ let trayIcon = new Tray(path.join(__dirname, 'assets/icons/icon.png'))
 
          let trayMenu = Menu.buildFromTemplate(trayMenuTemplate)
          trayIcon.setContextMenu(trayMenu)
+         trayIcon.setToolTip('PiQture')
